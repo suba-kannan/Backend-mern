00127a80f2da756b00aba4de412b01372840b8c4
suba-kannan <subakannan1409@gmail.com>
2026-02-25 22:15:15 +0530

    Create loop day 3files

---
+++ Loop/sum.js
@@ -0,0 +1,14 @@
+// const sumNumbers = (a,sum)=>{
+//   for (i = 0; i <= a; i++) {
+//     sum += i ;
+//   }
+//   console.log(`Total sum: ${sum}`);
+// }
+
+// sumNumbers(5,0);
+
+let sum = 0;
+for (i = 0; i <= 5; i++) {
+  sum += i;
+}
+console.log(`Total sum: ${sum}`);
